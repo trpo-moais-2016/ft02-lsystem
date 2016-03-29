e8073bc505171720bbf13e3c0f347b4f1adf1f65
DoriDruz <knower13@gmail.com>
2016-03-30 00:27:48 +0500

    Почти наверное работает

--- TASK IS HERE/l2system.js
+++ TASK IS HERE/l2system.js
@@ -3,16 +3,15 @@ function Point(x,y){
   this.y = y;
 }
 function parseString(){
-  var example = document.getElementById("cvs"),
-  ctx = example.getContext('2d');
+  var canvas = document.getElementById("cvs"),
+  ctx = canvas.getContext('2d');
   ctx.fillStyle="#9999ff";
-  ctx.fillRect(0, 0, example.width, example.height);
 
-  var stepLength = 20;
-  var startPoint = new Point(350, 400); //new Point(x, y); function parseString(rule, x, y, angle)
+  var stepLength = 10;
+  var startPoint = new Point(50, 600);
   var newPoint;
   var step = 4;
-  var angle = -Math.PI/4;
+  var angle = -Math.PI/3;
   var turnAngle = Math.PI/8;
   var rule = "F";
   var newRule = "-F+F+[+F-F-]-[-F+F+F]";
@@ -20,19 +19,17 @@ function parseString(){
   var savePositionsY = [];
   var saveAngle = [];
 
-  ctx.beginPath();
   for(var i = 0; i < step; i++){
-    //console.log("Step " + i);
-    //console.log(rule);
+    console.log("Step " + i);
+    console.log(rule);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
     for(var j = 0; j < rule.length; j++){
-      //console.log("step is " + rule[j]);
       switch (rule[j]){
         case 'F':
           ctx.moveTo(startPoint.x, startPoint.y);
-          //console.log("Zero Point is ", startPoint.x, startPoint.y);
           newPoint = new Point(startPoint.x + (stepLength)*Math.cos(angle), startPoint.y + (stepLength)*Math.sin(angle));
           ctx.lineTo(newPoint.x, newPoint.y);
-          //console.log("Point is ", newPoint.x, newPoint.y);
           startPoint = newPoint;
           break;
         case '-':
@@ -45,22 +42,19 @@ function parseString(){
           savePositionsX.push(startPoint.x);
           savePositionsY.push(startPoint.y);
           saveAngle.push(angle);
-          ctx.moveTo(startPoint.x, startPoint.y)
-          //console.log("SAVE ", startPoint.x, startPoint.y);
           break;
         case ']':
           startPoint.x = savePositionsX.pop();
           startPoint.y = savePositionsY.pop();
           angle = saveAngle.pop();
           ctx.moveTo(startPoint.x, startPoint.y)
-          //console.log("LOAD ", startPoint.x, startPoint.y);
           break;
         default:
           console.log("Something wrong");
           break;
       }
     }
-    rule = rule.replace(/F/g,newRule)  //rule.replace(new RegExp("F",'g'),newRule);
+    rule = rule.replace(/F/g,newRule)
+    ctx.stroke();
   }
-  ctx.stroke();
 }
